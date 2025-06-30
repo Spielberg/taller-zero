9e2fb0194159b7c2643330f0de6971d6c22629ae
Fco Javier Sánchez Ostiz <javier.sanchez@jakala.com>
2025-06-30 14:50:43 +0200

    Agrega un proxy para la URL de la API en el archivo de configuración.

--- config.ts
+++ config.ts
@@ -1,2 +1,3 @@
-export const apiUrl = 'http://dataservice.accuweather.com';
+export const proxyUrl = 'https://cors-anywhere.herokuapp.com';
+export const apiUrl = `${proxyUrl}/http://dataservice.accuweather.com`;
 export const apiKey = '6jDXaov1AArEbe94sqAGIzKT2A0MkT14';
