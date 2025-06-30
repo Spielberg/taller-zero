8761f9bc50e448b04c84afdbc89775c12b4047f6
Fco Javier Sánchez Ostiz <javier.sanchez@jakala.com>
2025-06-30 14:53:37 +0200

    Cambia el proxy de la URL de la API en el archivo de configuración a un nuevo servicio.

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const proxyUrl = 'https://cors-anywhere.herokuapp.com';
-export const apiUrl = `${proxyUrl}/http://dataservice.accuweather.com`;
+export const proxyUrl = 'https://api.allorigins.win/get?url=';
+export const apiUrl = `${proxyUrl}http://dataservice.accuweather.com`;
 export const apiKey = '6jDXaov1AArEbe94sqAGIzKT2A0MkT14';
